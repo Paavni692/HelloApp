2d14aed63e4cfb96dc14f55ca6a95d4a52fee147
paavni'git config --global user.email 'pc7705@srmist.edu.ingit config --global user.name paavnigit config --global user.email pc7705@srmist.edu.in <pc7705@srmist.edu.in>
2026-03-17 13:53:18 +0530

    Display Hello with command line argument or default message

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,11 @@
 public class HelloApp {
-    public static void main(String[] args){
-        String name = "World";
+    public static void main(String [] args){
 
-        System.out.println("Hello, " + name + "!");
+        String name = "Paavni";
+        if(args.length > 0){
+            name = args[0];
+        }
+
+        System.out.println("Hi, " + name + "!");
     }
 }
