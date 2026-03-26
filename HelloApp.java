6ead5be6edb9eb1cba9e587e14e08226e854fc18
paavni'git config --global user.email 'pc7705@srmist.edu.ingit config --global user.name paavnigit config --global user.email pc7705@srmist.edu.in <pc7705@srmist.edu.in>
2026-03-26 13:23:56 +0530

    Display Hello with multiple args using String.join() method

--- HelloApp.java
+++ HelloApp.java
@@ -1,17 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
-
-        StringBuilder nameBuilder = new StringBuilder();
-
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         }
     }
